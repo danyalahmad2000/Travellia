88d2783af6fdc6a37c6c3543bc33cf444db746c0
danyalahmad2000 <danyal.ahmad.ez@gmail.com>
2024-07-11 23:47:28 +0500

    Stays Complete

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -36,13 +36,13 @@ const Search = () => {
   }, []);
 
   return (
-    <div className="container mx-auto bg-white rounded-tl-xl rounded-3xl">
+    <div className="container mx-auto bg-white rounded-tl-xl rounded-3xl p-4 xl:p-8">
       <div className="border border-solid flex flex-col bg-white rounded-3xl">
-        <div className="flex flex-row justify-between items-center w-[500px] h-[40px] mx-auto">
+        <div className="flex flex-wrap justify-center xl:justify-between items-center w-full xl:w-[500px] h-[40px] mx-auto">
           {menuItems.map((item) => (
             <h1
               key={item.id}
-              className={`text-[16px] font-semibold cursor-pointer relative ${
+              className={`text-[14px] xl:text-[16px] font-semibold cursor-pointer relative mx-2 xl:mx-0 ${
                 clickedItem === item.id ? "text-[#D9B748]" : "text-gray-600"
               }`}
               onClick={() => handleClick(item.id)}
@@ -60,23 +60,23 @@ const Search = () => {
             </h1>
           ))}
         </div>
-        <div className="bg-gray-300 h-[1px] w-full"></div>
+        <div className="bg-gray-300 h-[1px] w-full mt-2 xl:mt-4"></div>
 
         {clickedItem === 1 && (
           <div className="flex flex-col pb-5">
-            <div className="flex flex-row px-10 h-[100px] items-center justify-between">
-              <Location/>
+            <div className="flex flex-wrap xl:flex-nowrap px-4 xl:px-10 h-auto xl:h-[100px] items-center justify-start xl:justify-between gap-4 xl:gap-0 mt-4 xl:mt-0">
+              <Location />
 
               <Dates />
 
-              <Passengers/>
+              <Passengers />
 
-              <button className="w-[100px] h-[50px] rounded-3xl border border-solid bg-[#D9B748] text-white font-semibold">
+              <button className="w-full xl:w-[100px] h-[50px] rounded-3xl border border-solid bg-[#D9B748] text-white font-semibold">
                 Search
               </button>
             </div>
 
-            <div className="flex flex-row items-center ml-10 gap-6">
+            <div className="flex flex-col xl:flex-row items-start xl:items-center ml-4 xl:ml-10 gap-4 xl:gap-6 mt-4 xl:mt-0">
               <div className="flex items-center gap-2">
                 <Checkbox
                   className="hover:border-[#D9B748] cursor-pointer"
--- src/components/search/search-components/Passengers.jsx
+++ src/components/search/search-components/Passengers.jsx
@@ -5,6 +5,7 @@ const Passengers = () => {
   const [showPassengers, setShowPassengers] = useState(false);
   const [passengerCounts, setPassengerCounts] = useState({
     adults: 1,
+    youth: 0,
     children: 0,
     infants: 0,
   });
@@ -33,27 +34,25 @@ const Passengers = () => {
     <>
       <div className="relative">
         <div
-          className="flex flex-row w-[300px] border border-solid border-gray-600 rounded-xl h-[50px] items-center px-5 cursor-pointer"
+          className="flex flex-row w-[350px] border border-solid border-gray-600 rounded-xl h-[50px] items-center px-5 cursor-pointer"
           onClick={togglePassengers}
         >
           <FaUser className="w-5 h-5 mr-5" />
           <div className="flex flex-col">
             <p className="text-[12px] text-gray-500">Travellers</p>
             <p className="text-[16px] text-gray-700">
-              {passengerCounts.adults} Adult
-              {passengerCounts.adults !== 1 && "s"}, {passengerCounts.children}{" "}
-              Child{passengerCounts.children !== 1 && "ren"},{" "}
-              {passengerCounts.infants} Infant
-              {passengerCounts.infants !== 1 && "s"}
+              {passengerCounts.adults} Adult{passengerCounts.adults !== 1 && "s"}, 
+              {" "}{passengerCounts.youth} Youth,
+              {" "}{passengerCounts.children}{" "} Child{passengerCounts.children !== 1 && "ren"},
+              {" "}{passengerCounts.infants} Infant{passengerCounts.infants !== 1 && "s"}
             </p>
           </div>
-          
         </div>
         {showPassengers && (
-          <div className="absolute mt-2 w-full lg:w-[300px] bg-white shadow-lg rounded-lg z-20 top-14">
+          <div className="absolute mt-2 w-full lg:w-[350px] bg-white shadow-lg rounded-lg z-20 top-14">
             <div className="p-4">
               <div className="flex items-center justify-between mb-4">
-                <p className="font-bold">Adults</p>
+              <p className="font-semibold">Adults <span className="text-gray-500 font-normal">( More than 12 yrs )</span></p>
                 <div className="flex items-center space-x-2">
                   <button
                     className="px-3 py-1 bg-gray-200 rounded"
@@ -71,7 +70,25 @@ const Passengers = () => {
                 </div>
               </div>
               <div className="flex items-center justify-between mb-4">
-                <p className="font-bold">Children</p>
+                <p className="font-semibold">Youth <span className="text-gray-500 font-normal">( 11 yrs - 15 yrs )</span></p>
+                <div className="flex items-center space-x-2">
+                  <button
+                    className="px-3 py-1 bg-gray-200 rounded"
+                    onClick={() => decreaseCount("youth")}
+                  >
+                    -
+                  </button>
+                  <span>{passengerCounts.infants}</span>
+                  <button
+                    className="px-3 py-1 bg-gray-200 rounded"
+                    onClick={() => increaseCount("youth")}
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+              <div className="flex items-center justify-between mb-4">
+              <p className="font-semibold">Children <span className="text-gray-500 font-normal">( 02 yrs - 11 yrs )</span></p>
                 <div className="flex items-center space-x-2">
                   <button
                     className="px-3 py-1 bg-gray-200 rounded"
@@ -89,7 +106,7 @@ const Passengers = () => {
                 </div>
               </div>
               <div className="flex items-center justify-between">
-                <p className="font-bold">Infants</p>
+              <p className="font-semibold">Infants <span className="text-gray-500 font-normal">( Less than 02 yrs )</span></p>
                 <div className="flex items-center space-x-2">
                   <button
                     className="px-3 py-1 bg-gray-200 rounded"
